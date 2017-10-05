659db42a9d64d8f52086efc2de0f311c176e05a8
Leonardo Santana <leonardogsantana@live.com>
2017-10-05 09:28:31 -0300

    caminho relativo do json

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -7,7 +7,8 @@ var i = 0;
 var hiddenWord = '';
 
 App.controller('HangmanCtrl', function($scope, $http) {
-    $http.get('https://leonardogsantana.github.io/paises.json')
+    //$http.get('https://leonardogsantana.github.io/paises.json')
+    $http.get('./paises.json')
         .then(function(res){
         $scope.registros = res.data.paises;
         $scope.todosPaises = res.data;
